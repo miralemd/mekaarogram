6eca24c766b0036445361bb39e49eb3cce87ced5
Miralem Drek <mek@qlik.com>
2015-10-19 11:44:02 +0200

    Add gulp uglify task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require( 'gulp' );
 var eslint = require( 'gulp-eslint' );
 var sass = require( 'gulp-sass' );
 var changed = require( 'gulp-changed' );
+var uglify = require( 'gulp-uglify' );
 
 var qdir = (process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'] ) + "\\Documents\\Qlik\\Sense\\Extensions\\dendrogram";
 
@@ -28,7 +29,14 @@ gulp.task( 'copy', function () {
 		.pipe( gulp.dest( qdir ) );
 } );
 
+gulp.task( 'compress', function() {
+	return gulp.src( ['./src/**/*.js', '!./src/js/external/*.js'])
+		.pipe( uglify() )
+		.pipe( gulp.dest( qdir ) );
+} );
+
 gulp.task( 'watch', function () {
 	gulp.watch( ['./src/**/*.*'], ['lint', 'sass', 'copy'] );
 } );
 
+gulp.task( 'build', ['lint', 'sass', 'copy', 'compress'] );
