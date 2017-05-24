d7ef56b16e0e83cb7b80deed823d3b0eeb688d7b
Miralem Drek <mek@qlik.com>
2017-05-24 13:12:51 +0200

    Include wbl file in build

--- webpack.config.js
+++ webpack.config.js
@@ -17,17 +17,23 @@ var output = path.resolve( __dirname, "dist" );
 
 var qdirpath = [os.homedir(), "Qlik", "Sense", "Extensions", qname];
 if( os.platform() === "win32" ) {
-       	qdirpath.splice( 1, 0, "Documents" );
+	qdirpath.splice( 1, 0, "Documents" );
 }
 var qdir = path.resolve.apply( path, qdirpath );
 
 var isWatching = yargs.argv.watch;
 
 function onBuild() {
- 	cpy( [qname + ".qext", "external/*.*", "assets/*.png", "css/*.ttf"], output, {
- 		cwd: srcDir,
-    parents: true
- 	} ).then( function() {
+	cpy( [
+		qname + ".qext",
+		"*.wbl",
+		"external/*.*",
+		"assets/*.png",
+		"css/*.ttf"
+	], output, {
+		cwd: srcDir,
+		parents: true
+	} ).then( function() {
 		cpy( ["./**/*.*"], qdir, {
 			cwd: output,
 			parents: true
@@ -35,61 +41,59 @@ function onBuild() {
 	} );
 }
 
-if( false ) {
-  console.log( "is watching" );
-       	watch.watchTree( "./node_modules/@qlik/picasso/dist/", function( fn ) {
-    console.log( "detected" );
-       		onBuild();
-       	} );
-}
+// if( false ) {
+// 	console.log( "is watching" );
+// 	watch.watchTree( "./node_modules/@qlik/picasso/dist/", function( fn ) {
+// 		console.log( "detected" );
+// 		onBuild();
+// 	} );
+// }
 
 var config = {
-       	entry: entry,
-       	output: {
-       		path: output,
-       		filename: qnamejs,
-       		libraryTarget: "amd"
-       	},
-       	module: {
-       		loaders: [{
-       			test: /\.js$/,
-       			loader: "babel-loader",
-       			query: {
-       				presets: ["es2015"]
-       			}
-       		},
-          {
-            test: /\.scss$/,
-            loader: "css-loader?-url!sass-loader"
-          },
-          {
-            test: /\.html$/,
-            loader: "svg-inline"
-          }]
-       	},
-       	externals: [
-       		{
-       			"default-view": "objects.extension/default-view",
-            "translator": "translator",
-            "object-conversion": "objects.extension/object-conversion",
-            "components": "client.property-panel/components/components",
-            "pivot-api": "objects.backend-api/pivot-api",
-            "require": "require",
-            "qvangular": "qvangular",
-            "jquery": "jquery",
-            "color": "general.utils/color",
-            "default-view": "objects.extension/default-view",
-            "selection-toolbar": "objects.extension/default-selection-toolbar",
-            "event-utils": "objects.utils/event-utils",
-            "state": "client.utils/state",
-            "tooltip-service": "objects.views/charts/tooltip/chart-tooltip-service",
-            "touche": "touche",
-            "d3": "./external/d3"
-       		}
-       	],
-       	plugins: [
-       		new WebPackOnBuild( onBuild )
-       	]
+	entry: entry,
+	output: {
+		path: output,
+		filename: qnamejs,
+		libraryTarget: "amd"
+	},
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			loader: "babel-loader",
+			query: {
+				presets: ["es2015"]
+			}
+		},
+		{
+		test: /\.scss$/,
+		loader: "css-loader?-url!sass-loader"
+		},
+		{
+		test: /\.html$/,
+		loader: "svg-inline"
+		}]
+	},
+	externals: [{
+		"default-view": "objects.extension/default-view",
+		"translator": "translator",
+		"object-conversion": "objects.extension/object-conversion",
+		"components": "client.property-panel/components/components",
+		"pivot-api": "objects.backend-api/pivot-api",
+		"require": "require",
+		"qvangular": "qvangular",
+		"jquery": "jquery",
+		"color": "general.utils/color",
+		"default-view": "objects.extension/default-view",
+		"selection-toolbar": "objects.extension/default-selection-toolbar",
+		"event-utils": "objects.utils/event-utils",
+		"state": "client.utils/state",
+		"tooltip-service": "objects.views/charts/tooltip/chart-tooltip-service",
+		"touche": "touche",
+		"d3": "./external/d3"
+	}],
+	plugins: [
+		new WebPackOnBuild( onBuild )
+	]
 };
 
 module.exports = config;
